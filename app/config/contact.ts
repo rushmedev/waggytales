57fecd16d1c9b1d0a4fdb9d93c69ed7d7cea35d6
rushmedev <rushmedev@gmail.com>
2026-09-05 08:28:18 +0530

    Change register button to phone link

--- app/config/contact.ts
+++ app/config/contact.ts
@@ -8,7 +8,7 @@ export const contactInfo = {
   callNumber: "09705241131",
   dialerUrl: "tel:09705241131",
   whatsappUrl: buildWhatsAppUrl("Hi M & M Waggy Tales, I want to know more."),
-  registerNowUrl: "https://pettleapp.com/booknow/fc0db379-e032-5954-00ac-a00e6eba44b7",
+  registerNowUrl: "tel:09705241131",
   googleMapsUrl: "https://maps.app.goo.gl/kMudCZpQU24WpRqr9",
   googleMapsEmbedUrl:
     "https://www.google.com/maps?output=embed&q=M+%26+M+Waggy+Tales,+Golden+Tulip+Colony,+Hyderabad,+Telangana&z=17",
